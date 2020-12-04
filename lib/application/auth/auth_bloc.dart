16784226e0237272ea37613fc29fe951a5b817df
Sangam Kumar Shrestha <2shrestha22@gmail.com>
2020-12-04 19:14:25 +0545

    auth bloc

--- lib/application/auth/auth_bloc.dart
+++ lib/application/auth/auth_bloc.dart
@@ -1,19 +1,36 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:firebase_ddd/domain/auth/i_auth_facade.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
 part 'auth_bloc.freezed.dart';
 
+@injectable
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(const Initial());
+  AuthBloc(this._authFacade) : super(const Initial());
+
+  final IAuthFacade _authFacade;
 
   @override
   Stream<AuthState> mapEventToState(
     AuthEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    yield* event.map(
+      authCheckRequested: (e) async* {
+        final userOption = await _authFacade.getSignedInUser();
+        userOption.fold(
+          () => const AuthState.authenticated(),
+          (_) => const AuthState.authenticated(),
+        );
+      },
+      signedOut: (e) async* {
+        await _authFacade.signOut();
+        yield const AuthState.unAuthenticated();
+      },
+    );
   }
 }
